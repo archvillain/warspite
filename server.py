2e88fddd9ea4aaf0fdd18ae693ccf89d85afc2b9
Jimi Som <johnjimysom@gmail.com>
2018-08-24 23:40:32 -0700

    added and revised server.py for the server

---
+++ server.py
@@ -0,0 +1,62 @@
+#author: Johnjimy Som
+#date: 2018年8月24日
+#version: 1.0.0
+#objective: これはRSh Scriptのチュートリアルです 
+
+import socket
+import sys
+
+def socket_create():
+    try: 
+        global host 
+        global port 
+        global s
+        host = ''
+        port = 0420 #try not to use the common port (for testing)
+        s = socket.socket()
+    except socket.error as msg:
+        print("socket creation error: " +str(msg))
+
+
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+        s.bind((host, port))
+        s.listen(3)
+    except socket.error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "retrying.. its prolly your fault lol")
+        print("Socket binding error: " + str(msg) + "\n" + "check if you have your port: " + str(port) + "on still lol.")
+        print("Socket binding error: " + str(msg) + "\n" + "try turning your terminal off and on.. lol")
+        socket_bind() 
+
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established |" + "IP: " + address[0] + " | Port: " + str(address[0]))
+    send_commands(conn)
+    conn.close()
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8") #when we get the response its gonna be in bytes so we have to convert to string. buffer 1024
+            print(client_response,'\n') #'\n' or (pip3 ->)end="" dont give us a new line character at the end
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
+
+#debug
+#input()
+print(sys.version) #check your version on python
